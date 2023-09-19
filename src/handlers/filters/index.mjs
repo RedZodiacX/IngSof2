3af163f22fbc746089a53789a1be8159c688c943
RedZodiacX <locurax41@hotmail.com>
2023-09-19 15:06:40 -0500

    Add files via upload

--- src/handlers/filters/index.mjs
+++ src/handlers/filters/index.mjs
@@ -1,14 +1,32 @@
 import { Router } from "express";
 import applyFiltersHandler from "./applyFiltersHandler.mjs";
+import multer from "multer";
 
 const router = Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.get("/", (req, res) => {
     res.send("ok imagen get");
 });
 
-router.post("/", applyFiltersHandler);
+router.post("/", upload.array("files"), (req, res) => {
+    const files = req.files;
+    const filters = req.body.filters;
 
-export const test = () => {}
+    console.log("Filters:", filters);
+
+    files.forEach((file, index) => {
+        console.log(`File ${index + 1}:`);
+        console.log("Fieldname:", file.fieldname);
+        console.log("Originalname:", file.originalname);
+        console.log("Mimetype:", file.mimetype);
+    });
+
+    res.send("Archivos recibidos y procesados correctamente.");
+});
+
+export const test = () => {};
 
 export default router;
