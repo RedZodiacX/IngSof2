f9524150e276d49fe0909d5cec2a31e2c23339f3
JMartin27 <martinvasquez_gf@hotmail.com>
2023-11-15 09:56:07 -0500

    commons y models

--- src/models/process.mjs
+++ src/models/process.mjs
@@ -1,6 +1,25 @@
 import { Schema, model } from 'mongoose';
-import { TYPE_OF_FILTERS } from '../commons/constans.mjs';
+import { TYPE_OF_FILTERS, STATUS_TYPES } from '../commons/constans.mjs';
 
+const filterSchema = new Schema(
+  {
+    name: String,
+    status: {
+      type: String,
+      enum: STATUS_TYPES,
+      default: 'in-progress',
+      required: true,
+    },
+    imageUrl: String,
+    message: String,
+  },
+);
+const imageSchema = new Schema(
+  {
+    imageUrl: String,
+    filters: [filterSchema],
+  },
+);
 const processSchema = new Schema(
   {
     filters: {
@@ -12,12 +31,11 @@ const processSchema = new Schema(
         },
       ],
     },
+    images: [imageSchema],
   },
   {
     timestamps: true,
   },
 );
-
 const ProcessModel = model('Process', processSchema);
-
 export default ProcessModel;
